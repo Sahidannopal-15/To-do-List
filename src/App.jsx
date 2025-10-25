0b333abc88c59a42088e87f84dfcc2720effceec
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-10-25 12:50:04 +0700

    Menambahkan fitur tanggal dan jam

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   let [kegiatan, setKegiatan] = useState([]);
   let [input, setInput] = useState("");
   let [tema, setTema] = useState("dark");
-  
+  let [waktu, setWaktu] = useState("");
+  let [tanggal, setTanggal] = useState ("");
+
   function gantiTema(){
     setTema(tema == "dark" ? "light" : "dark");
   }
@@ -16,10 +18,17 @@ function App() {
   }
   function isiInput(){
     if(input == ""){
-      alert ("Kosong, Pak!")
+      alert ("Isi kegiatannya dulu, Pak!")
+    }else if(waktu == ""){
+      alert ("Isi Tanggal dulu, Pak!")
     }else{
-      setKegiatan([...kegiatan,input]);
+      let kegiatanBaru = {
+        Nama: input,
+        Waktu: waktu
+      };
+      setKegiatan([...kegiatan,kegiatanBaru]);
       setInput("");
+      setWaktu("");
     }
   }
   function hapusKegiatan(index){
@@ -33,16 +42,18 @@ function App() {
               Ganti Tema</button>
       <section className='flex flex-col gap-3 p-4 items-center'>
       <header className=''>Daftar Kegiatan : </header>
-          <input 
-        placeholder='Masukan Kegiatan Kamu....'
-        type="text"
-        value = {input}
-        onChange={updateInput}
-        className='px-2 py-2 rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-400'/>
-        <button className='mx-1 py-2 px-4 bg-purple-800 text-white rounded-lg hover:bg-purple-500 transition-all duration-500'
-                onClick={isiInput}>
-          Tambah
-        </button>
+        <input 
+          placeholder='Masukan Kegiatan Kamu....'
+          type="text"
+          value = {input}
+          onChange={updateInput}
+          className='px-2 py-2 rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-400'/>
+        <input type="date" />
+        <input type="time" />
+          <button className='mx-1 py-2 px-4 bg-purple-800 text-white rounded-lg hover:bg-purple-500 transition-all duration-500'
+                    onClick={isiInput}>
+              Tambah
+          </button>
       <ul className='w-72'>
         {kegiatan.map((item,index) =>(
         <li key={index}
